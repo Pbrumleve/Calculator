be15767bff6078cbc4ec0dab7c2ef81dd411028b
pbrumleve <paul.brumleve@gmail.com>
2024-03-17 22:59:59 -0500

    edited divide function

--- main.js
+++ main.js
@@ -18,15 +18,29 @@ buttons.forEach((button) => {
 });
 
 function populateDisplay(input) {
+  const content = document.querySelector('.display')
   if (input === 'clear') {
     clearDisplay();
     console.log(numOne);
+  } else if (input === 'backspace') {
+    if (operator === '') {
+      numOne = numOne.slice(0, -1);
+      console.log(numOne);
+    } else if (numTwo === '') {
+      operator = '';
+      console.log(operator);
+    } else {
+      numTwo = numTwo.slice(0, -1);
+      console.log(numTwo);
+    };
   } else if (input === '=' && numOne !== '' && numTwo !== '' && operator !== '') {
     operate(numOne, numTwo, operator);
     console.log(answer);
-    clearDisplay()
+    numOne = answer;
+    numTwo = '';
+    operator = '';
   } else if (input === '=' && (numOne === '' || numTwo === '')) {
-    console.log('Please select a number followed by an operator followed by a number before clicking equals. Try again!')
+    console.log('Please have a number followed by an operator followed by a number chosen before clicking equals. Try again!')
     clearDisplay()
   } else {
     if (operator === '') {
@@ -36,12 +50,12 @@ function populateDisplay(input) {
       } else if (input === '+' || input === '-' || input === '*' || input === '/') {
         operator = input;
         console.log(operator);
-      } else {console.log('You cannot have two decimal points!')};
+      } else {console.log('Invalid input!')};
     } else {
       if (input === '0' || input === '1' || input === '2' || input === '3' || input === '4' || input === '5' || input === '6' || input === '7' || input === '8' || input === '9' || (input === '.' && !numTwo.includes('.'))) {
         numTwo += input;
         console.log(numTwo);
-      } else {console.log('You cannot have two decimal points!')};
+      } else {console.log('Invalid input!')};
     };
   };
 };
@@ -50,6 +64,7 @@ function clearDisplay() {
   numOne = '';
   numTwo = '';
   operator = '';
+  answer = '';
 }
 
 function operate(numOne, numTwo, operator) {
@@ -88,5 +103,15 @@ function multiply(x, y) {
 };
 
 function divide(x, y) {
-  return answer = x / y;
+  if (y === 0) {
+    return console.log(`Nice try!`)
+  } else {
+  answer = x / y;
+  return round(answer);
+  }
+};
+
+function round(num) {
+  let stepOne = 1000 * num;
+  return Math.round(stepOne) / 1000;
 };
